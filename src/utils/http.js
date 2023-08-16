1c0a7d0e78f4472f08f5eeb755a2859733fa0c64
彭子洪 <1454700819@qq.com>
2023-08-16 23:07:21 +0800

    登陆功能实现

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,8 +1,10 @@
 //axios 的封转
 import axios from "axios";
- const  httpInstance=  axios.create({
-  baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-  timeout:5000
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+const httpInstance = axios.create({
+  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  timeout: 5000
 })
 
 //拦截器
@@ -13,6 +15,11 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  //统一错误提示
+  ElMessage({
+    type: 'warning',
+    message: e.response.data.msg
+  })
   return Promise.reject(e)
 })
 
